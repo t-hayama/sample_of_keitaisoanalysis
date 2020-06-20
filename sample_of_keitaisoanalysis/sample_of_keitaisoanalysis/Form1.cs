d0bff24d2ceb2c1373ca2592d885d7789754d86b
knowledge-media <57474456+knowledge-media@users.noreply.github.com>
2020-06-21 05:06:53 +0900

    first

---
+++ sample_of_keitaisoanalysis/sample_of_keitaisoanalysis/Form1.cs
@@ -0,0 +1,54 @@
+﻿using NMeCab;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace sample_of_keitaisoanalysis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.KeyDown += this.tb_KeyDown;
+        }
+
+        private void tb_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                //形態素解析されるもとの文章
+                string text = richTextBox1.Text;// "私はプログラマーです";
+
+                var tagger = MeCabTagger.Create();
+
+                //形態素解析を行い結果を記録
+                string result = tagger.Parse(text);
+
+                richTextBox2.Text = result;
+
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //形態素解析されるもとの文章
+            string text = richTextBox1.Text;// "私はプログラマーです";
+
+            var tagger = MeCabTagger.Create();
+
+            //形態素解析を行い結果を記録
+            string result = tagger.Parse(text);
+
+            richTextBox2.Text = result;
+
+
+        }
+    }
+}
